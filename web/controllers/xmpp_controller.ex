e1c9e21245a1daf3c818ddd60433fdbc4d7611be
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-10-12 21:26:28 -0500

    minor code cleanup

--- web/controllers/xmpp_controller.ex
+++ web/controllers/xmpp_controller.ex
@@ -4,14 +4,12 @@ defmodule Xebex.XmppController do
   require Record
   Record.defrecord :jid, Record.extract(:jid, from: "deps/ejabberd/include/jlib.hrl")
 
-  #plug :action
-
   def index(conn, _params) do
     # get online jid, parse and extract the user part.
     online_users = :ejabberd_sm.connected_users
                       |> Enum.map &(jid(:jlib.string_to_jid(&1), :user))    
 
-    json conn, %{users: online_users}
+    json conn, %{online_users: online_users}
   end
 
 
